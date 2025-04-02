5fba6fe2d1e469cdda1840b88870c49fe8bec97f
狐狸先生 <18438443537@163.com>
2025-04-02 23:56:14 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
  */
 public class Main {
     public static void main(String[] args) {
+        // This is a simple Java program that prints "Hello, World!" to the console.
         System.out.println("Hello, World!");
     }
 }
