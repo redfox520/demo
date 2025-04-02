7d876831c8a944c8704ee8921afeb804c88f7c91
护理先生 <lwqredfox520Z@gmail.com>
2025-04-02 23:49:02 +0800

    测试

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author 李文强
+ * @create 2025-04-02 23:48
+ * @package ${PACKAGE_NAME}.${NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
